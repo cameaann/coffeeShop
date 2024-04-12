fd0ccde980b5b001c981ef7f2b4a17d9876bab23
Anna <cameaann@gmail.com>
2024-04-12 14:23:21 +0300

    initial project

---
+++ src/test/java/coffeeShop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package coffeeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

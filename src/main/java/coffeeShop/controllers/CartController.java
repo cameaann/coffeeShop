65b583432355810264054d1a23d30174e0023d81
Anna <cameaann@gmail.com>
2024-05-17 11:12:38 +0300

    creating CartController

--- src/main/java/coffeeShop/controllers/CartController.java
+++ src/main/java/coffeeShop/controllers/CartController.java
@@ -1,2 +1,51 @@
-package coffeeShop.controllers;public class CartController {
+package coffeeShop.controllers;
+
+import coffeeShop.models.Cart;
+import coffeeShop.models.CartItem;
+import coffeeShop.services.CartService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+
+@Controller
+public class CartController {
+
+    private CartService cartService;
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @GetMapping("/cart")
+    public String viewCart(Model model){
+        Cart cart = cartService.getCart();
+        model.addAttribute("cart", cart);
+        return "public/cart";
+    }
+
+    @PostMapping("/cart")
+    public String addProduct(@RequestParam long depId,
+                             @RequestParam long productId,
+                             @RequestParam String name,
+                             @RequestParam BigDecimal price){
+        cartService.addProduct(productId, name, price);
+        return "redirect:/products?id="+depId;
+    }
+
+
+    @PostMapping("/cart/increase/{productId}")
+    public String increaseQuantity(@PathVariable long productId){
+        cartService.increase(productId);
+        return "redirect:/cart";
+    }
+    @PostMapping("/cart/decrease/{productId}")
+    public String decreaseQuantity(@PathVariable long productId){
+        cartService.decrease(productId);
+        return "redirect:/cart";
+    }
+
 }
